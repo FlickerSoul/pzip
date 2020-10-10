1b12b25571abbe42713ea7e92812bb869f429fba
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-10-10 15:30:51 -0700

    Update test_task_queue.c

--- test_task_queue.c
+++ test_task_queue.c
@@ -16,6 +16,7 @@ void test_task_node_create_and_delete() {
     assert(task_node_ptr->write_data_queue_position == position);
 
     destroy_task_node(&task_node_ptr);
+    assert(task_node_ptr == NULL);
 }
 
 void test_data_composition() {
@@ -35,11 +36,35 @@ void test_data_composition() {
     FILE* f = fopen("data_composition_out.o", "w");
     fwrite(test_data, sizeof(int) + sizeof(char), 1, f);
     fclose(f);
-
+    
     free(test_data);
 }
 
+void* data_gen() {
+    void* test_data = malloc(sizeof(int) + sizeof(char));
+    void* counter = test_data;
+    *(uint32_t*)counter = 38;
+    counter += sizeof(int);
+    *(char*)counter = 'a';
+
+    return test_data;
+}
+
 void test_write_data_create_and_delete() {
+    void* chunck = data_gen();
+    char first = 'b';
+    char last = 'c';
+    int first_count = 10;
+    int last_count = 20;
+    write_data_t* write_data = create_write_data(chunck, first, first_count, last, last_count);
+    assert(write_data->main_data == chunck);
+    assert(write_data->first_char = first);
+    assert(write_data->last_char = last);
+    assert(write_data->first_count = first_count);
+    assert(write_data->last_count = last_count);
+
+    destroy_write_data(&write_data);
+    assert(write_data == NULL);
 }
 
 void test_enqueue() {
@@ -53,4 +78,5 @@ void test_parallel_enqueue() {
 int main() {
     test_task_node_create_and_delete();
     test_data_composition();
+    test_write_data_create_and_delete();
 }
