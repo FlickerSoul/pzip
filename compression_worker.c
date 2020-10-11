9a77fb24771d024214046fc88f1f3c3a1e3daa44
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-10-11 11:56:09 -0700

    Update compression_worker.c

--- compression_worker.c
+++ compression_worker.c
@@ -5,6 +5,60 @@
 #include "compression_worker.h"
 #include "stdio.h"
 
+write_data_t* compress_data(FILE* file) {
+
+    void* chunk = NULL;
+    int data_chunk_num = 0;
+
+    int first_char, last_char = 0;
+    uint32_t first_count, last_count = 0;
+
+    // first char
+    int c, last = first_char = fgetc(file);
+    int c_count = first_count = 0;
+
+    assert(last != EOF);
+
+    for (first_count = 1; (c = fgetc(file)) == last; first_count++) {
+        // empty
+    }
+
+    last = c;
+
+    if (last != EOF) {
+        chunk = malloc(WRITE_CHUNK_SIZE * CHUNK_SIZE);
+        void* temp = chunk;
+
+        while (last != EOF) {
+            for (c_count = 1; (c = fgetc(file)) == last; c_count++) {
+                // empty
+            }
+
+            *(uint32_t*)temp = c_count;
+            temp += UINT32_SIZE;
+            *(char*)temp = last;
+            temp += 1;
+
+            last = c;
+
+            data_chunk_num += 1;
+        }
+
+        last_char = *(char*)(temp-CHAR_SIZE);
+        last_count = *(uint32_t*)(temp-WRITE_CHUNK_SIZE);
+        data_chunk_num -= 1;
+
+        if (data_chunk_num == 0) {
+            free(chunk);
+            chunk = NULL;
+        }
+    }
+
+    write_data_t* data = create_write_data(chunk, first_char, first_count, last_char, last_count);
+
+    return data;
+}
+
 
 void* compression_worker(void* args) {
     while (1) {
@@ -28,54 +82,8 @@ void* compression_worker(void* args) {
         long offset = task_node->file_position * CHUNK_SIZE;
         fseek(file, offset, SEEK_SET);
 
-        void* chunk = NULL;
-        int data_chunk_num = 0;
+        write_data_t* data = compress_data(file);
 
-        int first_char, last_char = 0;
-        uint32_t first_count, last_count = 0;
-
-        // first char
-        int c, last = first_char = fgetc(file);
-        int c_count = first_count = 0;
-
-        assert(last != EOF);
-
-        for (first_count = 1; (c = fgetc(file)) == last; first_count++) {
-            // empty
-        }
-
-        last = c;
-
-        if (last != EOF) {
-            chunk = malloc(WRITE_CHUNK_SIZE * CHUNK_SIZE);
-            void* temp = chunk;
-
-            while (last != EOF) {
-                for (c_count = 1; (c = fgetc(file)) == last; c_count++) {
-                    // empty
-                }
-
-                *(uint32_t*)temp = c_count;
-                temp += UINT32_SIZE;
-                *(char*)temp = last;
-                temp += 1;
-
-                last = c;
-
-                data_chunk_num += 1;
-            }
-
-            last_char = *(char*)(temp-CHAR_SIZE);
-            last_count = *(uint32_t*)(temp-WRITE_CHUNK_SIZE);
-            data_chunk_num -= 1;
-
-            if (data_chunk_num == 0) {
-                free(chunk);
-                chunk = NULL;
-            }
-        }
-
-        write_data_t* data = create_write_data(chunk, first_char, first_count, last_char, last_count);
         pthread_mutex_lock(&write_queue_lock);
         put_data(data, task_node->write_data_queue_position);
         pthread_cond_signal(&write_queue_filled);
