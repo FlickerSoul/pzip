85c31b473f816f7a227a3bd2fcd46e5df01055fa
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-10-11 14:19:41 -0700

    Update test_workers.c

--- test_workers.c
+++ test_workers.c
@@ -24,12 +24,33 @@ void test_simple_compress_data() {
 }
 
 void test_complex_compress_data() {
+    FILE* f = fopen("tests/4.in", "r");
+    write_data_t* data = compress_data(f);
+    fclose(f);
 
+    assert(data->first_char == 'a');
+    assert(data->first_count == 58);
+
+    assert(data->main_data != NULL);
+    assert(data->data_chunk_num == 8);
+
+    void* temp = data->main_data;
+    uint32_t counts[8] = {1, 31, 1, 20, 1, 31, 1, 377};
+    char chars[8] = {'\n', 'b', '\n', 'c', '\n', 'd', '\n', 'e'};
+    for (int i = 0; i < data->data_chunk_num; i++) {
+        assert(*(uint32_t*)(temp) == counts[i]);
+        temp += sizeof(uint32_t);
+        assert(*(char*)(temp) == chars[i]);
+        temp += sizeof(char);
+    }
+
+    assert(data->last_char == '\n');
+    assert(data->last_count == 1);
 }
 
 int main() {
     test_get_chunk_num();
-    test_compress_data();
+    test_simple_compress_data();
     test_complex_compress_data();
     return 0;
 }
